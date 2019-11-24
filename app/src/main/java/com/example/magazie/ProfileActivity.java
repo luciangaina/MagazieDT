5e9e9e5452eb20f84d3b7c8baef63749bdd022b4
lucian <g.luci97@gmail.com>
2019-11-24 22:30:41 +0200

    login finished

--- app/src/main/java/com/example/magazie/ProfileActivity.java
+++ app/src/main/java/com/example/magazie/ProfileActivity.java
@@ -38,13 +38,10 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
-                /*
                 Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
-                 */
-                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
             }
         });
     }
