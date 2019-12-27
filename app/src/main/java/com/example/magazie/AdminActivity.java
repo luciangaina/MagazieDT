c6f9d59dc19130b2399b4fb55fa143bf7b70d19b
lucian <g.luci97@gmail.com>
2019-12-27 20:48:55 +0200

    admin page layout

--- app/src/main/java/com/example/magazie/AdminActivity.java
+++ app/src/main/java/com/example/magazie/AdminActivity.java
@@ -1,14 +1,38 @@
 package com.example.magazie;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AdminActivity extends AppCompatActivity {
+
+    private Button bttnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        bttnLogout = findViewById(R.id.buttonLogout);
+
+        bttnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(AdminActivity.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onCreateUser(View view) {
+        startActivity(new Intent(AdminActivity.this, SignUpActivity.class));
     }
 }
--- app/src/main/java/com/example/magazie/ProfileActivity.java
+++ app/src/main/java/com/example/magazie/ProfileActivity.java
@@ -73,7 +73,7 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
